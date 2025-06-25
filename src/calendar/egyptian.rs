a79edb59b89db79017fa1141eadb9284b4f1159b
ax <>
2025-06-25 03:33:29 +0000

    NON_MONTH for Egyptian
    FossilOrigin-Name: 524d509e8997a4e855a3b0245bdba1d6eefe0e6871318610855f80f7a5fd5bca

--- src/calendar/egyptian.rs
+++ src/calendar/egyptian.rs
@@ -18,6 +18,7 @@ use num_traits::FromPrimitive;
 use std::num::NonZero;
 
 const NABONASSAR_ERA_JD: i32 = 1448638;
+const NON_MONTH: u8 = 13;
 
 #[derive(Debug, PartialEq, PartialOrd, Clone, Copy, FromPrimitive)]
 pub enum EgyptianMonth {
@@ -49,7 +50,7 @@ pub struct Egyptian(CommonDate);
 
 impl Egyptian {
     pub fn complementary(self) -> Option<EgyptianDaysUponTheYear> {
-        if self.0.month == 13 {
+        if self.0.month == NON_MONTH {
             EgyptianDaysUponTheYear::from_u8(self.0.day)
         } else {
             None
@@ -100,13 +101,13 @@ impl ToFromCommonDate for Egyptian {
     }
 
     fn valid_month_day(date: CommonDate) -> Result<(), CalendarError> {
-        if date.month < 1 || date.month > 13 {
+        if date.month < 1 || date.month > NON_MONTH {
             Err(CalendarError::InvalidMonth)
         } else if date.day < 1 {
             Err(CalendarError::InvalidDay)
-        } else if date.month < 13 && date.day > 30 {
+        } else if date.month < NON_MONTH && date.day > 30 {
             Err(CalendarError::InvalidDay)
-        } else if date.month == 13 && date.day > 5 {
+        } else if date.month == NON_MONTH && date.day > 5 {
             Err(CalendarError::InvalidDay)
         } else {
             Ok(())
@@ -114,14 +115,14 @@ impl ToFromCommonDate for Egyptian {
     }
 
     fn year_end_date(year: i32) -> CommonDate {
-        CommonDate::new(year, 13, 5)
+        CommonDate::new(year, NON_MONTH, 5)
     }
 }
 
 impl Quarter for Egyptian {
     fn quarter(self) -> NonZero<u8> {
         let m = self.to_common_date().month as u8;
-        if m == 13 {
+        if m == NON_MONTH {
             NonZero::new(4 as u8).expect("4 != 0")
         } else {
             NonZero::new(((m - 1) / 3) + 1).expect("(m - 1) / 3 > -1")
