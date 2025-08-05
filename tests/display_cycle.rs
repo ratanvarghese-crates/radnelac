5546a1df885aac1e41dc146f49c07bf6ad7da640
ax <>
2025-08-05 07:04:11 +0000

    Test changes
    FossilOrigin-Name: 737fc2c3732d1c75d19d1890135a3603a3231f02a9bab39352413f69e26df642

---
+++ tests/display_cycle.rs
@@ -0,0 +1,36 @@
+#[cfg(feature = "display")]
+mod display_logic {
+    pub use proptest::proptest;
+    pub use radnelac::day_count::BoundedDayCount;
+    pub use radnelac::day_count::Fixed;
+    pub use radnelac::day_count::FromFixed;
+    pub use radnelac::day_count::FIXED_MAX;
+    pub use radnelac::day_cycle::Akan;
+    pub use radnelac::day_cycle::BoundedCycle;
+    pub use radnelac::day_cycle::Weekday;
+
+    pub fn repeats<const N: u8, const M: u8, T: BoundedCycle<N, M> + FromFixed + ToString>(x: f64) {
+        let f1 = Fixed::new(x);
+        let a1 = T::from_fixed(f1);
+        let a2 = T::from_fixed(Fixed::new(f1.get() + 1.0));
+        let a3 = T::from_fixed(Fixed::new(f1.get() + (T::cycle_length() as f64)));
+        assert_ne!(a1.to_string(), a2.to_string());
+        assert_eq!(a1.to_string(), a3.to_string());
+    }
+}
+
+#[cfg(feature = "display")]
+use display_logic::*;
+
+#[cfg(feature = "display")]
+proptest! {
+    #[test]
+    fn weekday_display_repeats(x in (-FIXED_MAX)..(FIXED_MAX - 7.0)) {
+        repeats::<7, 0, Weekday>(x);
+    }
+
+    #[test]
+    fn akan_display_repeats(x in (-FIXED_MAX)..(FIXED_MAX - 7.0)) {
+        repeats::<42, 1, Akan>(x);
+    }
+}
---
+++ tests/display_exact.rs
@@ -0,0 +1,134 @@
+#[cfg(feature = "display")]
+mod display_logic {
+    pub use proptest::proptest;
+    pub use radnelac::calendar::Armenian;
+    pub use radnelac::calendar::Coptic;
+    pub use radnelac::calendar::Cotsworth;
+    pub use radnelac::calendar::Egyptian;
+    pub use radnelac::calendar::Ethiopic;
+    pub use radnelac::calendar::FrenchRevArith;
+    pub use radnelac::calendar::Gregorian;
+    pub use radnelac::calendar::Holocene;
+    pub use radnelac::calendar::Julian;
+    pub use radnelac::calendar::Positivist;
+    pub use radnelac::calendar::Symmetry010;
+    pub use radnelac::calendar::Symmetry010Solstice;
+    pub use radnelac::calendar::Symmetry454;
+    pub use radnelac::calendar::Symmetry454Solstice;
+    pub use radnelac::day_count::BoundedDayCount;
+    pub use radnelac::day_count::Fixed;
+    pub use radnelac::day_count::FromFixed;
+    pub use radnelac::day_count::FIXED_MAX;
+    pub use radnelac::display::Language;
+    pub use radnelac::display::PresetDisplay;
+    pub use radnelac::display::PresetFormat;
+    pub use radnelac::display::COMPL_ONLY;
+    pub use radnelac::display::HHMMSS_COLON;
+
+    pub fn display_exact<T: FromFixed + PresetDisplay>(t: f64, fmt: PresetFormat, s: &str) {
+        let d = T::from_fixed(Fixed::new(t));
+        let ds = d.preset_str(Language::EN, fmt);
+        assert_eq!(ds, s)
+    }
+
+    pub fn display_midnight<T: FromFixed + PresetDisplay>(t: f64) {
+        display_exact::<T>(t, HHMMSS_COLON, "00:00:00");
+    }
+
+    pub fn display_blank_complementary<T: FromFixed + PresetDisplay>(t: f64) {
+        display_exact::<T>(t, COMPL_ONLY, "");
+    }
+}
+
+#[cfg(feature = "display")]
+use display_logic::*;
+
+#[cfg(feature = "display")]
+proptest! {
+    #[test]
+    fn armenian_midnight(t0 in -FIXED_MAX..FIXED_MAX) {
+        display_midnight::<Armenian>(t0);
+    }
+
+    #[test]
+    fn coptic_midnight(t0 in -FIXED_MAX..FIXED_MAX) {
+        display_midnight::<Coptic>(t0);
+    }
+
+    #[test]
+    fn cotsworth_midnight(t0 in -FIXED_MAX..FIXED_MAX) {
+        display_midnight::<Cotsworth>(t0);
+    }
+
+    #[test]
+    fn egyptian_midnight(t0 in -FIXED_MAX..FIXED_MAX) {
+        display_midnight::<Egyptian>(t0);
+    }
+
+    #[test]
+    fn ethiopic_midnight(t0 in -FIXED_MAX..FIXED_MAX) {
+        display_midnight::<Ethiopic>(t0);
+    }
+
+    #[test]
+    fn ethiopic_complementary(t0 in -FIXED_MAX..FIXED_MAX) {
+        display_blank_complementary::<Ethiopic>(t0);
+    }
+
+    #[test]
+    fn french_rev_midnight(t0 in -FIXED_MAX..FIXED_MAX) {
+        display_midnight::<FrenchRevArith<false>>(t0);
+        display_midnight::<FrenchRevArith<true>>(t0);
+    }
+
+    #[test]
+    fn gregorian_midnight(t0 in -FIXED_MAX..FIXED_MAX) {
+        display_midnight::<Gregorian>(t0);
+    }
+
+    #[test]
+    fn gregorian_complementary(t0 in -FIXED_MAX..FIXED_MAX) {
+        display_blank_complementary::<Gregorian>(t0);
+    }
+
+    #[test]
+    fn holocene_midnight(t0 in -FIXED_MAX..FIXED_MAX) {
+        display_midnight::<Holocene>(t0);
+    }
+
+    #[test]
+    fn holocene_complementary(t0 in -FIXED_MAX..FIXED_MAX) {
+        display_blank_complementary::<Holocene>(t0);
+    }
+
+    #[test]
+    fn julian_midnight(t0 in -FIXED_MAX..FIXED_MAX) {
+        display_midnight::<Julian>(t0);
+    }
+
+    #[test]
+    fn julian_complementary(t0 in -FIXED_MAX..FIXED_MAX) {
+        display_blank_complementary::<Julian>(t0);
+    }
+
+    #[test]
+    fn positivist_midnight(t0 in -FIXED_MAX..FIXED_MAX) {
+        display_midnight::<Positivist>(t0);
+    }
+
+    #[test]
+    fn symmetry_midnight(t0 in -FIXED_MAX..FIXED_MAX) {
+        display_midnight::<Symmetry010>(t0);
+        display_midnight::<Symmetry454>(t0);
+        display_midnight::<Symmetry010Solstice>(t0);
+        display_midnight::<Symmetry454Solstice>(t0);
+    }
+
+    #[test]
+    fn symmetry_complementary(t0 in -FIXED_MAX..FIXED_MAX) {
+        display_blank_complementary::<Symmetry010>(t0);
+        display_blank_complementary::<Symmetry454>(t0);
+        display_blank_complementary::<Symmetry010Solstice>(t0);
+        display_blank_complementary::<Symmetry454Solstice>(t0);
+    }
+}
