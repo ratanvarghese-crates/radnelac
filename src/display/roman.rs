33598a469df2837dd63335e76fc22b9f0dd56422
ax <>
2025-08-05 04:42:53 +0000

    Additional testing for Roman
    FossilOrigin-Name: 8ef3e7e36d6cb5cc4007745cb959cd35a487e1a9f8c5b3cfba62d9050f920a0c

--- src/display/roman.rs
+++ src/display/roman.rs
@@ -65,3 +65,53 @@ impl fmt::Display for Roman {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::calendar::CommonDate;
+    use crate::calendar::Julian;
+    use crate::calendar::ToFromCommonDate;
+    use crate::day_count::FromFixed;
+    use crate::day_count::ToFixed;
+
+    #[test]
+    fn second_sixth_day_before_kalends_of_march() {
+        let j24 = Julian::try_from_common_date(CommonDate::new(4, 2, 24)).unwrap();
+        let j25 = Julian::try_from_common_date(CommonDate::new(4, 2, 25)).unwrap();
+        let f24 = j24.to_fixed();
+        let f25 = j25.to_fixed();
+        let r24s = Roman::from_fixed(f24).to_string();
+        let r25s = Roman::from_fixed(f25).to_string();
+        assert!(r24s.starts_with("ante diem VI Kalends March"), "{}", r24s);
+        assert!(
+            r25s.starts_with("ante diem bissextum VI Kalends March"),
+            "{}",
+            r25s
+        );
+    }
+
+    #[test]
+    fn pridie_nones_of_march() {
+        let j = Julian::try_from_common_date(CommonDate::new(-44, 3, 6)).unwrap();
+        let f = j.to_fixed();
+        let r = Roman::from_fixed(f);
+        assert!(r.to_string().starts_with("pridie Nones March"));
+    }
+
+    #[test]
+    fn pridie_ides_of_march() {
+        let j = Julian::try_from_common_date(CommonDate::new(-44, 3, 14)).unwrap();
+        let f = j.to_fixed();
+        let r = Roman::from_fixed(f);
+        assert!(r.to_string().starts_with("pridie Ides March"));
+    }
+
+    #[test]
+    fn ides_of_march() {
+        let j = Julian::try_from_common_date(CommonDate::new(-44, 3, 15)).unwrap();
+        let f = j.to_fixed();
+        let r = Roman::from_fixed(f);
+        assert!(r.to_string().starts_with("Ides of March"));
+    }
+}
